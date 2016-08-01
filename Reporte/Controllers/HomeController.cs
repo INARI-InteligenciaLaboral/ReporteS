a314a14907470758e37eb31e81594b7bbb1e8c06
Santiago Iván Almaraz Segovia <salmaraz@inteligencialaboral.com>
2016-08-01 17:09:42 -0500

    Se agrego vista index y entrega de proyecto

--- Reporte/Controllers/HomeController.cs
+++ Reporte/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 //using Excel = Microsoft.Office.Interop.Excel;
@@ -15,8 +16,15 @@ namespace Reporte.Controllers
 {
     public class HomeController : Controller
     {
+        public bool m_importe = false;
+        public bool m_mensual = false;
         // GET: Home
         public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: Home
+        public ActionResult FilterMensual()
         {
             DataTable m_Empresas = SqlClass.sqldata.ObtenerEmp();
             List<SelectListItem> l_empresas = new List<SelectListItem>();
@@ -53,15 +61,9 @@ namespace Reporte.Controllers
                     Value = Row[0].ToString()
                 });
             }
-            List<SelectListItem> l_anos = new List<SelectListItem>();
-            l_anos = SqlClass.ListasStaticas.ObtenerAnos();
-            List<SelectListItem> l_meses = new List<SelectListItem>();
-            l_meses = SqlClass.ListasStaticas.ObtenerMeses();
             var model = new CascadingDropdownsModel
             {
-                Empresas = l_empresas,
-                Anos = l_anos,
-                Meses = l_meses
+                Empresas = l_empresas
             };
             return View(model);
         }
@@ -88,71 +90,122 @@ namespace Reporte.Controllers
                 Text = "Todos los procesos",
                 Value = sop_proceso
             });
-            MultiSelectList m_List = new MultiSelectList(l_Procesos.OrderBy(i => i.Value), "Value", "Text");
+            MultiSelectList m_List = new MultiSelectList(l_Procesos, "Value", "Text");
             var procesos = l_Procesos;
             return Json(m_List, JsonRequestBehavior.AllowGet);
         }
         
         public ActionResult GenerarDocument([Bind(Include = "SelectedEmpresas,SelectedAnos,SelectedProcesos,SelectedMeses")] CascadingDropdownsModel m_Results)
         {
+            while(m_mensual)
+            {
+                Thread.Sleep(5000);
+            }
+            m_mensual = true;
             DataTable m_result = SqlClass.sqldata.GenerarReporte(m_Results);
-            //WriteExcelWithNPOI(m_result,"xlsx");
+            WriteExcelWithNPOI(m_result, m_Results.SelectedAnos + "-" + m_Results.SelectedEmpresas);
+            m_mensual = false;
             return Redirect("Index");
         }
 
-        //public void WriteExcelWithNPOI(DataTable dt, String extension)
-        //{
+        public ActionResult GenerarDocumentPro([Bind(Include = "SelectedEmpresas,SelectedProcesos,SelectedMeses")] CascadingDropdownsModel m_Results)
+        {
+            while (m_importe)
+            {
+                Thread.Sleep(5000);
+            }
+            m_importe = true;
+            DataTable m_result = SqlClass.sqldata.GenerarReporteImp(m_Results);
+            WriteExcelWith(m_result, m_Results.SelectedMeses + "-" + m_Results.SelectedEmpresas);
+            m_importe = false;
+            return Redirect("FilterProcesos");
+        }
+        
+        public void WriteExcelWithNPOI(DataTable dt, String m_nombre)
+        {
 
-        //    IWorkbook workbook;
+            IWorkbook workbook;
+            
+            workbook = new XSSFWorkbook();
 
-        //    if (extension == "xlsx")
-        //    {
-        //        workbook = new XSSFWorkbook();
-        //    }
-        //    else
-        //    {
-        //        throw new Exception("This format is not supported");
-        //    }
+            ISheet sheet1 = workbook.CreateSheet("Sheet 1");
 
-        //    ISheet sheet1 = workbook.CreateSheet("Sheet 1");
+            //make a header row
+            IRow row1 = sheet1.CreateRow(0);
 
-        //    //make a header row
-        //    IRow row1 = sheet1.CreateRow(0);
+            for (int j = 0; j < dt.Columns.Count; j++)
+            {
 
-        //    for (int j = 0; j < dt.Columns.Count; j++)
-        //    {
+                ICell cell = row1.CreateCell(j);
+                String columnName = dt.Columns[j].ToString();
+                cell.SetCellValue(columnName);
+            }
 
-        //        ICell cell = row1.CreateCell(j);
-        //        String columnName = dt.Columns[j].ToString();
-        //        cell.SetCellValue(columnName);
-        //    }
+            //loops through data
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                IRow row = sheet1.CreateRow(i + 1);
+                for (int j = 0; j < dt.Columns.Count; j++)
+                {
 
-        //    //loops through data
-        //    for (int i = 0; i < dt.Rows.Count; i++)
-        //    {
-        //        IRow row = sheet1.CreateRow(i + 1);
-        //        for (int j = 0; j < dt.Columns.Count; j++)
-        //        {
+                    ICell cell = row.CreateCell(j);
+                    String columnName = dt.Columns[j].ToString();
+                    cell.SetCellValue(dt.Rows[i][columnName].ToString());
+                }
+            }
 
-        //            ICell cell = row.CreateCell(j);
-        //            String columnName = dt.Columns[j].ToString();
-        //            cell.SetCellValue(dt.Rows[i][columnName].ToString());
-        //        }
-        //    }
+            using (var exportData = new MemoryStream())
+            {
+                Response.Clear();
+                workbook.Write(exportData);
+                Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                Response.AddHeader("Content-Disposition", string.Format("attachment;filename={0}", m_nombre + "Reporte.xlsx"));
+                Response.BinaryWrite(exportData.ToArray());
+                Response.End();
+            }
+        }
+        public void WriteExcelWith(DataTable dt, String m_nombre)
+        {
 
-        //    using (var exportData = new MemoryStream())
-        //    {
-        //        Response.Clear();
-        //        workbook.Write(exportData);
-        //        if (extension == "xlsx") //xlsx file format
-        //        {
-        //            Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
-        //            Response.AddHeader("Content-Disposition", string.Format("attachment;filename={0}", "SIAP.xlsx"));
-        //            Response.BinaryWrite(exportData.ToArray());
-        //        }
-        //        Response.End();
-        //    }
-        //}
+            IWorkbook workbook;
 
+            workbook = new XSSFWorkbook();
+
+            ISheet sheet1 = workbook.CreateSheet("Sheet 1");
+
+            //make a header row
+            IRow row1 = sheet1.CreateRow(0);
+
+            for (int j = 0; j < dt.Columns.Count; j++)
+            {
+
+                ICell cell = row1.CreateCell(j);
+                String columnName = dt.Columns[j].ToString();
+                cell.SetCellValue(columnName);
+            }
+
+            //loops through data
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                IRow row = sheet1.CreateRow(i + 1);
+                for (int j = 0; j < dt.Columns.Count; j++)
+                {
+
+                    ICell cell = row.CreateCell(j);
+                    String columnName = dt.Columns[j].ToString();
+                    cell.SetCellValue(dt.Rows[i][columnName].ToString());
+                }
+            }
+
+            using (var exportData = new MemoryStream())
+            {
+                Response.Clear();
+                workbook.Write(exportData);
+                Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                Response.AddHeader("Content-Disposition", string.Format("attachment;filename={0}", m_nombre + "Reporte.xlsx"));
+                Response.BinaryWrite(exportData.ToArray());
+                Response.End();
+            }
+        }
     }
 }
--- Reporte/SqlClass/sqldata.cs
+++ Reporte/SqlClass/sqldata.cs
@@ -13,10 +13,9 @@ namespace Reporte.SqlClass
 {
     public class sqldata
     {
-
         public static DataTable ObtenerEmp()
         {
-            string m_cadena = "Persist Security Info=False;User ID=sa;Password=Hardstyle1292;Initial Catalog=Siap_pruebas;Data Source=HANCOCK\\HANCOCKBOA";
+            string m_cadena = "Persist Security Info=False;User ID=usuario_siap;Password=lop35.qs;Initial Catalog=SIAP;";
             DataTable m_empresas = new DataTable();
             try
             {
@@ -36,14 +35,14 @@ namespace Reporte.SqlClass
 
         public static DataTable ObtenerPro(string m_empresa)
         {
-            string m_cadena = "Persist Security Info=False;User ID=sa;Password=Hardstyle1292;Initial Catalog=Siap_pruebas;Data Source=HANCOCK\\HANCOCKBOA";
+            string m_cadena = "Persist Security Info=False;User ID=usuario_siap;Password=lop35.qs;Initial Catalog=SIAP;";
             DataTable m_Procesos = new DataTable();
             try
             {
                 using (SqlConnection m_conexion = new SqlConnection(m_cadena))
                 {
                     m_conexion.Open();
-                    string m_command = "SELECT pro_keypro AS KeyPro, pro_despro AS DesPro FROM nmloproc WHERE pro_keycia = " + m_empresa;
+                    string m_command = "SELECT pro_keypro AS KeyPro, pro_despro AS DesPro FROM nmloproc WHERE pro_keycia = " + m_empresa + " ORDER BY KeyPro";
                     SqlCommand m_adapter = new SqlCommand(m_command, m_conexion);
                     m_Procesos.Load(m_adapter.ExecuteReader());
                     m_conexion.Close();
@@ -58,7 +57,7 @@ namespace Reporte.SqlClass
             SqlDataAdapter adp;
             DataSet ds = new DataSet();
             DataTable dt = new DataTable();
-            string m_cadena = "Persist Security Info=False;User ID=sa;Password=Hardstyle1292;Initial Catalog=Siap_pruebas;Data Source=HANCOCK\\HANCOCKBOA";
+            string m_cadena = "Persist Security Info=False;User ID=usuario_siap;Password=lop35.qs;Initial Catalog=SIAP;";
             try
             {
                 using (SqlConnection m_conexion = new SqlConnection(m_cadena))
@@ -66,7 +65,8 @@ namespace Reporte.SqlClass
                     m_conexion.Open();
                     SqlCommand cmd = new SqlCommand("P_T_Mensual", m_conexion);
                     cmd.CommandType = CommandType.StoredProcedure;
-                    cmd.Parameters.Add("@m_procesos", SqlDbType.Int, 10).Value = m_Parametros.SelectedProcesos;
+                    cmd.CommandTimeout = 0;
+                    cmd.Parameters.Add("@m_procesos", SqlDbType.NVarChar, 320).Value = m_Parametros.SelectedProcesos;
                     cmd.Parameters.Add("@m_mes", SqlDbType.NVarChar, 320).Value = m_Parametros.SelectedMeses;
                     cmd.Parameters.Add("@m_ano", SqlDbType.NVarChar, 320).Value = m_Parametros.SelectedAnos;
                     adp = new SqlDataAdapter(cmd);
@@ -76,7 +76,34 @@ namespace Reporte.SqlClass
                     m_conexion.Close();
                 }
             }
-            catch
+            catch (Exception ex)
+            { }
+            return dt;
+        }
+        public static DataTable GenerarReporteImp(CascadingDropdownsModel m_Parametros)
+        {
+            SqlDataAdapter adp;
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+            string m_cadena = "Persist Security Info=False;User ID=usuario_siap;Password=lop35.qs;Initial Catalog=SIAP;";
+            try
+            {
+                using (SqlConnection m_conexion = new SqlConnection(m_cadena))
+                {
+                    m_conexion.Open();
+                    SqlCommand cmd = new SqlCommand("P_T_Importe", m_conexion);
+                    cmd.CommandTimeout = 0;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@m_procesos", SqlDbType.NVarChar, 320).Value = m_Parametros.SelectedProcesos;
+                    cmd.Parameters.Add("@m_mes", SqlDbType.NVarChar, 320).Value = m_Parametros.SelectedMeses;
+                    adp = new SqlDataAdapter(cmd);
+                    ds = new DataSet();
+                    adp.Fill(ds);
+                    dt = ds.Tables[0];
+                    m_conexion.Close();
+                }
+            }
+            catch (Exception ex)
             { }
             return dt;
         }
